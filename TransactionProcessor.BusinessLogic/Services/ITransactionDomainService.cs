953626de67b8bc34ebec34b16628d230d24705b8
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2019-11-28 11:39:54 +0000

    Create Skeleton Service

---
+++ TransactionProcessor.BusinessLogic/Services/ITransactionDomainService.cs
@@ -0,0 +1,23 @@
+﻿namespace TransactionProcessor.BusinessLogic.Services
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Models;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ITransactionDomainService
+    {
+        #region Methods
+
+        /// <summary>
+        /// Processes the logon transaction.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
+        Task<ProcessLogonTransactionResponse> ProcessLogonTransaction(CancellationToken cancellationToken);
+
+        #endregion
+    }
+}
---
+++ TransactionProcessor/Factories/IModelFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace TransactionProcessor.Factories
+{
+    using DataTransferObjects;
+    using Models;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IModelFactory
+    {
+        #region Methods
+
+        /// <summary>
+        /// Converts from.
+        /// </summary>
+        /// <param name="processLogonTransactionResponse">The process logon transaction response.</param>
+        /// <returns></returns>
+        LogonTransactionResponse ConvertFrom(ProcessLogonTransactionResponse processLogonTransactionResponse);
+
+        #endregion
+    }
+}
