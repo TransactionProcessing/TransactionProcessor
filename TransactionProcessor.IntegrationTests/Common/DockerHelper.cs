29acf4825bf71d4ff4b5d938a17227762cb69097
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2022-10-22 13:13:35 +0100

    Update to use Balance Aggregator

--- TransactionProcessor.IntegrationTests/Common/DockerHelper.cs
+++ TransactionProcessor.IntegrationTests/Common/DockerHelper.cs
@@ -116,7 +116,7 @@
         public async Task PopulateSubscriptionServiceConfigurationGeneric(Boolean isSecureEventStore)
         {
             List<(String streamName, String groupName, Int32 maxRetries)> subscriptions = new List<(String streamName, String groupName, Int32 maxRetries)>();
-            subscriptions.Add(($"$ce-MerchantArchive", "Transaction Processor - Ordered", 0));
+            subscriptions.Add(($"$ce-MerchantBalanceArchive", "Transaction Processor - Ordered", 0));
             subscriptions.Add(($"$et-EstateCreatedEvent", "Transaction Processor - Ordered", 2));
             await this.PopulateSubscriptionServiceConfiguration(this.EventStoreHttpPort, subscriptions, isSecureEventStore);
         }
