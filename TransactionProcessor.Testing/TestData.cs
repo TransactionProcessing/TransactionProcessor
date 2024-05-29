d298772cb5246b96e6f104c94b176a245d65f2da
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2024-05-29 15:55:20 +0100

    fix unit tests

--- TransactionProcessor.Testing/TestData.cs
+++ TransactionProcessor.Testing/TestData.cs
@@ -327,7 +327,8 @@
                 ["SecurityConfiguration:Authority"] = "https://127.0.0.1",
                 ["AppSettings:EstateManagementApi"] = "http://127.0.0.1",
                 ["AppSettings:SecurityService"] = "http://127.0.0.1",
-                ["AppSettings:ContractProductFeeCacheExpiryInHours"] = ""
+                ["AppSettings:ContractProductFeeCacheExpiryInHours"] = "",
+                ["AppSettings:ContractProductFeeCacheEnabled"] = ""
             };
 
         public static EstateResponse GetEmptyEstateResponse =>
