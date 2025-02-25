efcbd844cc3d9a46483e17dc83e5314a0f076106
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2025-02-25 11:35:14 +0000

    Add retry logic for domain event handling
    Implement retry policies using Polly in the TransactionDomainEventHandler class. Update tests in TransactionDomainEventHandlerTests to cover new scenarios, including exception handling. Clean up commented-out test cases and update using directives for necessary namespaces.

--- TransactionProcessor.BusinessLogic.Tests/DomainEventHandlers/TransactionDomainEventHandlerTests.cs
+++ TransactionProcessor.BusinessLogic.Tests/DomainEventHandlers/TransactionDomainEventHandlerTests.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using EventStore.Client;
+using Grpc.Core;
+using MediatR;
 using SimpleResults;
 using TransactionProcessor.DomainEvents;
 
@@ -55,6 +57,39 @@ namespace TransactionProcessor.BusinessLogic.Tests.DomainEventHandlers
                 nameof(SettlementDomainEvents.MerchantFeeSettledEvent)=> new SettlementDomainEvents.MerchantFeeSettledEvent(TestData.SettlementAggregateId, TestData.EstateId, TestData.MerchantId, TestData.TransactionId, TestData.CalculatedFeeValue, 0, TestData.TransactionFeeId, TestData.TransactionFeeValue, TestData.TransactionFeeCalculateDateTime, TestData.SettlementDate),
                 nameof(TransactionDomainEvents.CustomerEmailReceiptRequestedEvent)=> TestData.CustomerEmailReceiptRequestedEvent,
                 nameof(TransactionDomainEvents.CustomerEmailReceiptResendRequestedEvent)=> TestData.CustomerEmailReceiptResendRequestedEvent,
+                _ => throw new NotSupportedException($"Event {eventType.Name} not supported")
+            };
+
+            var result = await this.TransactionDomainEventHandler.Handle(domainEvent, CancellationToken.None);
+            result.IsSuccess.ShouldBeTrue();
+        }
+
+        [Theory]
+        [InlineData(typeof(FloatDomainEvents.FloatCreditPurchasedEvent))]
+        [InlineData(typeof(TransactionDomainEvents.TransactionCostInformationRecordedEvent))]
+        //[InlineData(typeof(TransactionDomainEvents.TransactionHasBeenCompletedEvent))]
+        [InlineData(typeof(TransactionDomainEvents.MerchantFeePendingSettlementAddedToTransactionEvent))]
+        [InlineData(typeof(TransactionDomainEvents.SettledMerchantFeeAddedToTransactionEvent))]
+        [InlineData(typeof(SettlementDomainEvents.MerchantFeeSettledEvent))]
+        //[InlineData(typeof(CustomerEmailReceiptRequestedEvent))]
+        //[InlineData(typeof(CustomerEmailReceiptResendRequestedEvent))]
+        public async Task TransactionDomainEventHandler_EventPassedIn_Retry_EventIsHandled(Type eventType)
+        {
+            this.Mediator.SetupSequence(m => m.Send(It.IsAny<IRequest<Result>>(), It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new WrongExpectedVersionException("Stream1", StreamRevision.None, StreamRevision.None))
+                .ThrowsAsync(new RpcException(new Status(StatusCode.DeadlineExceeded, "Deadline Exceeded")))
+                .ReturnsAsync(Result.Success());
+
+            DomainEvent domainEvent = eventType.Name switch
+            {
+                nameof(FloatDomainEvents.FloatCreditPurchasedEvent) => new FloatDomainEvents.FloatCreditPurchasedEvent(TestData.FloatAggregateId, TestData.EstateId, TestData.CreditPurchasedDateTime, TestData.FloatCreditAmount, TestData.FloatCreditCostPrice),
+                nameof(TransactionDomainEvents.TransactionCostInformationRecordedEvent) => TestData.TransactionCostInformationRecordedEvent,
+                nameof(TransactionDomainEvents.TransactionHasBeenCompletedEvent) => TestData.DomainEvents.TransactionHasBeenCompletedEvent,
+                nameof(TransactionDomainEvents.MerchantFeePendingSettlementAddedToTransactionEvent) => new TransactionDomainEvents.MerchantFeePendingSettlementAddedToTransactionEvent(TestData.TransactionId, TestData.EstateId, TestData.MerchantId, TestData.CalculatedFeeValue, 0, TestData.TransactionFeeId, TestData.TransactionFeeValue, TestData.TransactionFeeCalculateDateTime, TestData.TransactionFeeSettlementDueDate, TestData.TransactionDateTime),
+                nameof(TransactionDomainEvents.SettledMerchantFeeAddedToTransactionEvent) => TestData.SettledMerchantFeeAddedToTransactionEvent(TestData.SettlementDate),
+                nameof(SettlementDomainEvents.MerchantFeeSettledEvent) => new SettlementDomainEvents.MerchantFeeSettledEvent(TestData.SettlementAggregateId, TestData.EstateId, TestData.MerchantId, TestData.TransactionId, TestData.CalculatedFeeValue, 0, TestData.TransactionFeeId, TestData.TransactionFeeValue, TestData.TransactionFeeCalculateDateTime, TestData.SettlementDate),
+                //nameof(TransactionDomainEvents.CustomerEmailReceiptRequestedEvent) => TestData.CustomerEmailReceiptRequestedEvent,
+                //nameof(TransactionDomainEvents.CustomerEmailReceiptResendRequestedEvent) => TestData.CustomerEmailReceiptResendRequestedEvent,
                 _ => throw new NotSupportedException($"Event {eventType.Name} not supported")
             };
 
