5927a6d6c194761b34bb879f83e7f4307681623d
StuartFerguson <stuart_ferguson1development@outlook.com>
2025-08-08 14:05:22 +0100

    unit test fixes

--- TransactionProcessor.BusinessLogic/Services/MerchantDomainService.cs
+++ TransactionProcessor.BusinessLogic/Services/MerchantDomainService.cs
@@ -399,20 +399,18 @@ namespace TransactionProcessor.BusinessLogic.Services
                         return Result.Invalid($"Merchant [{command.MerchantId}] has not made any deposits yet");
                     }
                     
-                    // TODO:Convert to use the new MerchantBalanceAggregate
-                    //// Now we need to check the merchants balance to ensure they have funds to withdraw
-                    //Result<String> getBalanceResult = await this.EventStoreContext.GetPartitionStateFromProjection("MerchantBalanceProjection", $"MerchantBalance-{command.MerchantId:N}", cancellationToken);
-                    //if (getBalanceResult.IsFailed)
-                    //{
-                    //    Result.Invalid($"Failed to get Merchant Balance.");
-                    //}
+                    Result<MerchantBalanceAggregate> getMerchantBalanceResult = await this.AggregateService.GetLatest<MerchantBalanceAggregate>(command.MerchantId, cancellationToken);
+                    Result<MerchantBalanceAggregate> merchantBalanceAggregateResult =
+                        DomainServiceHelper.HandleGetAggregateResult(getMerchantBalanceResult, command.MerchantId, false);
+                    if (merchantBalanceAggregateResult.IsFailed)
+                        return ResultHelpers.CreateFailure(merchantBalanceAggregateResult);
 
-                    //MerchantBalanceProjectionState1 projectionState = JsonConvert.DeserializeObject<MerchantBalanceProjectionState1>(getBalanceResult.Data);
+                    MerchantBalanceAggregate merchantBalanceAggregate = merchantBalanceAggregateResult.Data;
 
-                    //if (command.RequestDto.Amount > projectionState.merchant.balance)
-                    //{
-                    //    return Result.Invalid($"Not enough credit available for withdrawal of [{command.RequestDto.Amount}]. Balance is {projectionState.merchant.balance}");
-                    //}
+                    if (command.RequestDto.Amount > merchantBalanceAggregate.Balance)
+                    {
+                        return Result.Invalid($"Not enough credit available for withdrawal of [{command.RequestDto.Amount}]. Balance is {merchantBalanceAggregate.Balance}");
+                    }
 
                     // If we are here we have enough credit to withdraw
                     PositiveMoney amount = PositiveMoney.Create(Money.Create(command.RequestDto.Amount));
