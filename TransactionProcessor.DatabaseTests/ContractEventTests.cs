24a64da8a85ea01838240a16197b319a1b2832a1
StuartFerguson <stuart_ferguson1development@outlook.com>
2025-08-26 15:35:29 +0100

    operator duplicate handling

--- TransactionProcessor.DatabaseTests/ContractEventTests.cs
+++ TransactionProcessor.DatabaseTests/ContractEventTests.cs
@@ -13,6 +13,28 @@ using TransactionProcessor.Testing;
 
 namespace TransactionProcessor.DatabaseTests
 {
+    public class OperatorEventTests : BaseTest {
+        [Fact]
+        public async Task AddOperator_OperatorIsAdded()
+        {
+            Result result = await this.Repository.AddOperator(TestData.DomainEvents.OperatorCreatedEvent, CancellationToken.None);
+            result.IsSuccess.ShouldBeTrue();
+            EstateManagementContext context = this.GetContext();
+            Operator? @operator = await context.Operators.SingleOrDefaultAsync(c => c.OperatorId == TestData.DomainEvents.OperatorCreatedEvent.OperatorId);
+            @operator.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task AddOperator_OperatorIsAdded_EventReplayHandled()
+        {
+            Result result = await this.Repository.AddOperator(TestData.DomainEvents.OperatorCreatedEvent, CancellationToken.None);
+            result.IsSuccess.ShouldBeTrue();
+
+            result = await this.Repository.AddOperator(TestData.DomainEvents.OperatorCreatedEvent, CancellationToken.None);
+            result.IsSuccess.ShouldBeTrue();
+        }
+    }
+
     public class ContractEventTests : BaseTest
     {
         [Fact]
