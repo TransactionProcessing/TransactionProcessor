67cbce1f5988f3e00ca0f7303097deff82a0b84a
StuartFerguson <stuart_ferguson1development@outlook.com>
2025-08-26 18:31:36 +0100

    merge

--- TransactionProcessor.DatabaseTests/ContractEventTests.cs
+++ TransactionProcessor.DatabaseTests/ContractEventTests.cs
@@ -130,5 +130,25 @@ namespace TransactionProcessor.DatabaseTests
             result = await this.Repository.AddFileImportLog(TestData.DomainEvents.ImportLogCreatedEvent, CancellationToken.None);
             result.IsSuccess.ShouldBeTrue();
         }
-  }
+
+        [Fact]
+        public async Task AddFileImportLogFile_FileImportLogIsAdded()
+        {
+            Result result = await this.Repository.AddFileToImportLog(TestData.DomainEvents.FileAddedToImportLogEvent, CancellationToken.None);
+            result.IsSuccess.ShouldBeTrue();
+            EstateManagementContext context = this.GetContext();
+            var fileImportLogFile = await context.FileImportLogFiles.SingleOrDefaultAsync(f => f.FileImportLogId == TestData.DomainEvents.FileAddedToImportLogEvent.FileImportLogId && f.FileId == TestData.DomainEvents.FileAddedToImportLogEvent.FileId);
+            fileImportLogFile.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task AddFileImportLogFile_EventReplayHandled()
+        {
+            Result result = await this.Repository.AddFileToImportLog(TestData.DomainEvents.FileAddedToImportLogEvent, CancellationToken.None);
+            result.IsSuccess.ShouldBeTrue();
+
+            result = await this.Repository.AddFileToImportLog(TestData.DomainEvents.FileAddedToImportLogEvent, CancellationToken.None);
+            result.IsSuccess.ShouldBeTrue();
+        }
+    }
 }
