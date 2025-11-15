f3c0b96cf929df5ce47bc4bc6a660a0b40c11e4e
StuartFerguson <stuart_ferguson1development@outlook.com>
2025-11-15 19:39:22 +0000

    ...

--- TransactionProcessor.DataTransferObjects/SettlementResponse.cs
+++ TransactionProcessor.DataTransferObjects/SettlementResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace TransactionProcessor.DataTransferObjects
+﻿using Newtonsoft.Json;
+
+namespace TransactionProcessor.DataTransferObjects
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
@@ -6,16 +8,19 @@
     [ExcludeFromCodeCoverage]
     public class SettlementResponse
     {
+        [JsonProperty("estate_id")]
         public Guid EstateId { get; set; }
-
+        [JsonProperty("merchant_id")]
         public Guid MerchantId { get; set; }
 
+        [JsonProperty("settlement_date")]
         public DateTime SettlementDate { get; set; }
 
+        [JsonProperty("number_of_fees_pending_settlement")]
         public Int32 NumberOfFeesPendingSettlement { get; set; }
-
+        [JsonProperty("number_of_fees_settled")]
         public Int32 NumberOfFeesSettled { get; set; }
-
+        [JsonProperty("settlement_completed")]
         public Boolean SettlementCompleted { get; set; }
     }
 }
